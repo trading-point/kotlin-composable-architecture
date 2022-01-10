381d354ed63f633ba931544085ba163442e27d44
Theo Ioakeimidis <>
2022-01-10 17:37:41 +0200

    Adds Effects Basic example

--- examples/src/main/java/com/xm/examples/MainActivity.kt
+++ examples/src/main/java/com/xm/examples/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.xm.examples.databinding.ActivityMainBinding
 import com.xm.examples.fragments.CounterFragment
+import com.xm.examples.fragments.EffectsBasicFragment
 import com.xm.examples.fragments.TwoCountersFragment
 
 class MainActivity : AppCompatActivity(), Callback {
@@ -37,6 +38,13 @@ class MainActivity : AppCompatActivity(), Callback {
     override fun onTwoCountersClicked() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, TwoCountersFragment(), null)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onEffectsBasicClicked() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, EffectsBasicFragment(), null)
             .addToBackStack(null)
             .commit()
     }
---
+++ examples/src/main/java/com/xm/examples/utils/FactClientLive.kt
@@ -0,0 +1,23 @@
+package com.xm.examples.utils
+
+import android.util.Log
+import io.reactivex.rxjava3.core.Single
+import java.net.URL
+
+interface FactClient
+
+object FactClientLive : FactClient {
+
+    fun execute(number: String): Single<Result<String>> =
+        Single.fromCallable {
+            try {
+                val response = URL("http://numbersapi.com/$number/trivia").readText()
+                Result.Success(response)
+            } catch (e: Exception) {
+                Log.i("Exception", "execute: $e ")
+                // Sometimes numbersapi.com can be flakey, so if it ever fails we will just
+                // default to a mock response.
+                Result.Success("$number is a good number Brent")
+            }
+        }
+}
