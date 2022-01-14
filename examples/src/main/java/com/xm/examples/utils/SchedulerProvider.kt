cae8876976db254063f416bd1ce58bc23b000047
tioakeimidis <tioakeimidis@trading-point.com>
2022-01-14 22:25:31 +0200

    Adds unit tests

--- examples/src/main/java/com/xm/examples/utils/SchedulerProvider.kt
+++ examples/src/main/java/com/xm/examples/utils/SchedulerProvider.kt
@@ -1,19 +1,25 @@
 package com.xm.examples.utils
 
 import androidx.annotation.NonNull
-import androidx.annotation.Nullable
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.internal.schedulers.ImmediateThinScheduler
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 /**
  * Provides different types of schedulers.
  */
-object SchedulerProvider {
+class SchedulerProvider : BaseSchedulerProvider {
 
     @NonNull
-    fun io(): Scheduler = Schedulers.io()
+    override fun io(): Scheduler = Schedulers.io()
 
     @NonNull
-    fun mainThread(): Scheduler = AndroidSchedulers.mainThread()
+    override fun mainThread(): Scheduler = AndroidSchedulers.mainThread()
+}
+
+interface BaseSchedulerProvider {
+    fun io(): Scheduler
+
+    fun mainThread(): Scheduler
 }
---
+++ examples/src/test/java/com/xm/examples/01-GettingStarted-Counter-Test.kt
@@ -0,0 +1,32 @@
+package com.xm.examples
+
+import com.xm.examples.cases.CounterAction
+import com.xm.examples.cases.CounterEnvironment
+import com.xm.examples.cases.CounterState
+import com.xm.examples.cases.counterReducer
+import com.xm.tka.test.TestStore
+import com.xm.tka.test.TestStore.Step.Companion.send
+import org.junit.Test
+
+class GettingStartedCounterTest {
+
+    @Test
+    fun testCounter() {
+        val state = CounterState(count = 0)
+
+        TestStore(state, counterReducer, CounterEnvironment).assert(
+            send(CounterAction.IncrementButtonTapped) {
+                it.copy(count = 1)
+            },
+            send(CounterAction.IncrementButtonTapped) {
+                it.copy(count = 2)
+            },
+            send(CounterAction.DecrementButtonTapped) {
+                it.copy(count = 1)
+            },
+            send(CounterAction.DecrementButtonTapped) {
+                it.copy(count = 0)
+            }
+        )
+    }
+}
