a16f6b02fdf74d7efd8748c1f686cce40b0c36c3
Nasos Loukas <hey@nasos.dev>
2023-01-30 10:48:40 +0200

    Minor debugging additions

--- tka/src/main/kotlin/com/xm/tka/test/Debugging.kt
+++ tka/src/main/kotlin/com/xm/tka/test/Debugging.kt
@@ -7,10 +7,12 @@ import com.xm.tka.Reducer
  *
  * @param prefix: A string with which to prefix all debug messages.
  * @param printer: A print function. Defaults to a function that uses `println` function.
+ * @param actionsOnly: A boolean switch between action logging only or full log
  * @return A reducer that prints debug messages for all received actions.
  */
 fun <STATE, ACTION : Any, ENVIRONMENT> Reducer<STATE, ACTION, ENVIRONMENT>.debug(
     prefix: String = "",
+    actionsOnly: Boolean = false,
     printer: Printer = Printer()
 ): Reducer<STATE, ACTION, ENVIRONMENT> = Reducer { state, action, environment ->
     val pref = if (prefix.isEmpty()) prefix else prefix.plus(": ")
@@ -19,17 +21,35 @@ fun <STATE, ACTION : Any, ENVIRONMENT> Reducer<STATE, ACTION, ENVIRONMENT>.debug
             .also { (newState, _) ->
                 pref
                     .let {
-                        """
-                        ${it}Received:
-                        $it  State:   $state
-                        $it  Action:  $action
-                        $it  Reduced: $newState
-                        """.trimIndent()
+                        if (actionsOnly) {
+                            """
+                            ${it}Received: $action
+                            """.trimIndent()
+                        } else {
+                            """
+                            ${it}Received:
+                            $it  State:   $state
+                            $it  Action:  $action
+                            $it  Reduced: $newState
+                            """.trimIndent()
+                        }
                     }
                     .also(printer::print)
             }
     }.onFailure { printer.print("TKA: $prefix", it) }
         .getOrThrow()
+}
+
+/**
+ * [Reducer] extension to verify [com.xm.tka.Effect]s are dispatched on a main thread
+ */
+fun <STATE : Any, ACTION : Any, ENVIRONMENT : Any> Reducer<STATE, ACTION, ENVIRONMENT>.verifyDispatchOnMainThread(
+    mainThreadProvider: () -> Thread
+): Reducer<STATE, ACTION, ENVIRONMENT> = Reducer { state, action, environment ->
+    assert(Thread.currentThread() == mainThreadProvider()) {
+        "$action delivered on ${Thread.currentThread()}!!"
+    }
+    reduce(state, action, environment)
 }
 
 interface Printer {
